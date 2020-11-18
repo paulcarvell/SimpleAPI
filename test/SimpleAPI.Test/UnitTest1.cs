f9bb571ecc77870bd848331bce19a62a3a868621
paulcarvell <paul.carvell@soprasteria.com>
2020-11-18 12:01:10 +0000

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController(null);
+        [Fact]
+        public void GetReturnsShite()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Pile of shite",returnValue.Value);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+
+        }
+
+
+    }
+}
